516358480229abe624f39525e2d934479295a1ac
nickkabui <nickkabui@gmail.com>
2023-02-18 07:10:19 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,12 @@ int main(void)
 
 	for (a = 0; a < 10; a++)
 	{
-	putchar(a + '0');
-	if (a < '9')
-	{
-	putchar(',');
-	putchar(' ')
-	}
+		putchar(a + '0');
+		if (a < '9')
+		{
+			putchar(',');
+			putchar(' ')
+		}
 	}
 	putchar('\n');
 	return (0);
