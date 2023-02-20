971811a07c9b1c54ea54e4e0bd9041a7392167b7
nickkabui <nickkabui@gmail.com>
2023-02-20 09:37:10 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,10 +13,10 @@ int main(void)
 	for (a = '0'; a <= '9'; a++)
 	{
 
-	for (b = a + '1'; b <= '9'; b++)
+	for (b = a + 1; b <= '9'; b++)
 	{
 
-	for (c = b + '1'; c <= '9'; c++)
+	for (c = b + 1; c <= '9'; c++)
 	{
 		if ((b != a) != c)
 		{
