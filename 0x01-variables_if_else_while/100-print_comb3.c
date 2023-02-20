703d07ff42283dd28ee9216e635912673a67e5ce
nickkabui <nickkabui@gmail.com>
2023-02-20 08:19:32 -0800

    100-print_comb.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry Point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a, c;
+
+	for (a = '0'; a < '9'; a++)
+	{
+		
+	for (c = a+1; c <= '9'; c++)
+
+	{
+	if (c != a)
+	{
+	putchar(a);
+	putchar(c);
+	if (a == '8' && c == '9')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
