bb4a6147f6199ebde9afcf9b267d6f429e05fb45
nickkabui <nickkabui@gmail.com>
2023-02-23 07:46:16 -0800

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,12 +16,12 @@ int largest_number(int a, int b, int c)
 	{
 	largest = a;
 	}
-	else if (b > a && a > c)
+	else if (b > c && b > a)
 	{
 	largest = b;
 	}
 	else if (c > b)
-	{	
+	{
 	largest = c;
 	}
 	else
