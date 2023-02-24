9456a799f46cadb5e7888d02f99e7295996c561e
nickkabui <nickkabui@gmail.com>
2023-02-24 07:25:06 -0800

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Prints a Fizz Buzz program
+ * Returns: 0 (Success)
+ */
+int (void)
+
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if ((x % 3 == 0) && (x % 5 == 0))
+		{
+			printf("fizzBuzz");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("fizz");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", x);
+		}
+		if (x != 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
