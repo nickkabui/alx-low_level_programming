aea3440755740610ba22c5d7a0ee4f6b0d8a07de
nickkabui <nickkabui@gmail.com>
2023-02-24 04:43:51 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,12 @@
 #include "main.h"
+
 /**
  * more_numbers -  prints 10 times the numbers, from 0 to 14
  * Returns: 100 times the number from 0 to 14
  */
+
 void more_numbers(void)
+
 {
 	int a, b;
 
