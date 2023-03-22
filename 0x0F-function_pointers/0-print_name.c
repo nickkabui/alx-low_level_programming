bcf1852175ceabe00788ce35913ad3931394abc0
nickkabui <nickkabui@gmail.com>
2023-03-22 04:49:36 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ *  print_name - prints a name
+ *  @name: the name to print
+ *  @f: pointer to function
+ *  Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
